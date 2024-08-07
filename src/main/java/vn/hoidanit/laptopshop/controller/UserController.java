65f9fef3d4657b8730ffe7407a0692b01e072da1
VuManh01 <vutrongmanhmk@gmail.com>
2024-08-07 21:19:45 +0700

    version new 01

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,39 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+//     public String requestMethodName() {
+//         return new String();
+//     }
+
+//     public String getHomePage() {
+//         return "hello from controller";
+//     }
+// }
+
+@RestController
+public class UserController {
+
+    // DI: dependency ịnection
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+}
